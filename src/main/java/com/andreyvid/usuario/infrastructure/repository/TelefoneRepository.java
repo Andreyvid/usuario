4a7a9a5939cc0c48925499f27d3f6a32c7959c89
Andreyvid Dutra <andreyviddutra@gmail.com>
2025-11-15 15:56:09 -0300

    adicionadas classes de security

---
+++ src/main/java/com/andreyvid/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.andreyvid.usuario.infrastructure.repository;
+
+import com.andreyvid.iniciando_spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
